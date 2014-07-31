7b09bccc591c4df08b472c1ea4cd7a02a6643c90
Ben <bpruittvt@gmail.com>
2014-07-31 13:46:01 -0400

    update tests and test scripts

--- tests/runmauvetest.sh
+++ tests/runmauvetest.sh
@@ -2,23 +2,33 @@
 TESTNAME=$1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TESTDIR=${DIR}/${TESTNAME}_test_output
+TESTDIR=${DIR}/${TESTNAME}_output
 
-BASEGENOME=${DIR}/mds42_full.fa
-TESTGENOME=${DIR}/mds42_full_${TESTNAME}.fa
+BASESEQ=${DIR}/baseseq.fa
+TESTSEQ=${DIR}/test_${TESTNAME}.fa
 
-if [ ! -e ${TESTGENOME} ]
-  then
-    echo "${TESTNAME} is not a valid test; ${TESTGENOME} does not exist"
-    exit 1
+if [ ! -e ${TESTSEQ} ]; then
+  echo -e "\n${TESTNAME} is not a valid test; ${TESTSEQ} does not exist\n"
+  echo -e "Valid test names are: "
+  TESTRE="test_(.*).fa"
+  for f in $(ls ${DIR}); do
+    if [[ $f =~ $TESTRE ]]; then
+      test_name=${BASH_REMATCH[1]}
+      echo -e "\t${test_name}"
+    fi
+  done
+  echo -e
+  exit 1
 fi
-
 
 echo "Test output files will be saved to: ${TESTDIR}"
 
 rm -rf $TESTDIR
 mkdir $TESTDIR
 cd $TESTDIR
-$DIR/../bin/progressiveMauveStatic --output=mds42_full_${TESTNAME}.xmfa ${BASEGENOME} ${TESTGENOME}
+$DIR/../bin/progressiveMauveStatic --output=${TESTNAME}.xmfa ${BASESEQ} ${TESTSEQ}
+
+# Delete all of the .sslist files
+rm ${DIR}/*.sslist
 
 exit 0
--- tests/runswaptest.sh
+++
@@ -1,2 +0,0 @@
-
-../bin/progressiveMauveStatic --output=mds42_full_2swaps.xmfa mds42_full.fa mds42_full_2swaps.fa
